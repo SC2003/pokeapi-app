dce4eae8af2d645999601ff8ef4531d8c7202f88
SGurjar <sc8826752610@gmail.com>
2022-07-03 12:38:01 +0530

    src files

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     })
     return () => cancel()
   }, [currentPageUrl])
-  
+  if (loading) return "Loading..."
   const gotoNextPage = () => {
     setCurrentPageUrl(nextPageUrl)
   }
